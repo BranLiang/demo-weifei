9db3a5183af55abb1443c72f04f0678cbe6d6227
Bran Liang <lby89757@hotmail.com>
2018-09-21 21:24:08 +0800

    Add first graph

--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
 import React, { Component } from 'react';
+import {
+  Chart,
+  Geom,
+  Axis,
+  Tooltip,
+  Legend
+} from "bizcharts";
+import DataSet from "@antv/data-set";
+import zhongqiData from './data/zhongqi'
+
+const ds = new DataSet();
+/* Source data */
+ds.createView('zhongqi').source(zhongqiData)
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selected: 'zhongqi'
+    }
+  }
+
   render() {
+    const { selected } = this.state
+    const dv = ds.getView(selected)
+    dv.transform({
+      type: "fold",
+      fields: ["electricity", "water"],
+      key: "usage",
+      // key字段
+      value: "amount" // value字段
+    })
+
+    console.log(dv);
+
     return (
       <div>
-        Hello
+        <Chart height={400} data={dv} forceFit>
+          <Legend />
+          <Axis name="year" />
+          <Axis name="amount" />
+          <Tooltip
+            crosshairs={{
+              type: "y"
+            }}
+          />
+          <Geom
+            type="line"
+            position="year*amount"
+            size={2}
+            color={"usage"}
+            shape={"smooth"}
+          />
+          <Geom
+            type="point"
+            position="year*amount"
+            size={4}
+            shape={"circle"}
+            color={"usage"}
+            style={{
+              stroke: "#fff",
+              lineWidth: 1
+            }}
+          />
+        </Chart>
       </div>
     );
   }
---
+++ src/data/zhongqi.js
@@ -0,0 +1,63 @@
+// 中旗科技股份
+
+const data = [
+  {
+    year: 2015,
+    electricity: 14086584,
+    water: 417020,
+    waste: 721.54
+  },
+  {
+    year: 2016,
+    electricity: 14708430,
+    water: 224586,
+    waste: 1077.6
+  },
+  {
+    year: 2017,
+    electricity: 18936604,
+    water: 273647,
+    waste: 913.09
+  }
+]
+
+export default data;
+
+export const currentDate = [
+  {
+    month: 1,
+    electricity: 1960214,
+    water: 12680
+  },
+  {
+    month: 2,
+    electricity: 1733918,
+    water: 11625
+  },
+  {
+    month: 3,
+    electricity: 1661834,
+    water: 12891
+  },
+  {
+    month: 4,
+    electricity: 1928563,
+    water: 14193
+  },
+  {
+    month: 5,
+    electricity: 1936660,
+    water: 12154
+  },
+  {
+    month: 6,
+    electricity: 1787410,
+    water: 12594
+  },
+  {
+    month: 7,
+    electricity: 1099730,
+    water: 7426
+  }
+]
+
