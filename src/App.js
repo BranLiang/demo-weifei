e0fb054476a8fe75a4539d4c2fbf0d3b9f2d636e
Bran Liang <lby89757@hotmail.com>
2018-09-23 00:10:19 +0800

    Add percentage graph

--- src/App.js
+++ src/App.js
@@ -30,7 +30,47 @@ class App extends Component {
       callback(row) {
         row.wasteDivideElectricity = row.waste / row.electricity
         row.wasteDivideWater = row.waste / row.water
-        row.wasteDivieAll = row.waste / (row.electricity + row.water)
+        row.wasteDivideAll = row.waste / (row.electricity + row.water)
+        return row
+      }
+    })
+
+    const meanWasteDivideElectricity = dv.rows.reduce((sum, row) => {
+      return sum + row.wasteDivideElectricity
+    }, 0) / dv.rows.length
+    const meanWasteDivideWater = dv.rows.reduce((sum, row) => {
+      return sum + row.wasteDivideWater
+    }, 0) / dv.rows.length
+    const meanWasteDivideAll = dv.rows.reduce((sum, row) => {
+      return sum + row.wasteDivideAll
+    }, 0) / dv.rows.length
+
+    dv.transform({
+      type: 'map',
+      callback(row) {
+        row.meanWasteDivideElectricity = meanWasteDivideElectricity
+        row.meanWasteDivideWater = meanWasteDivideWater
+        row.meanWasteDivideAll = meanWasteDivideAll
+        return row
+      }
+    })
+
+    dv.transform({
+      type: 'map',
+      callback(row) {
+        row.meanWEDifference = row.wasteDivideElectricity - row.meanWasteDivideElectricity
+        row.meanWWDifference = row.wasteDivideWater - row.meanWasteDivideWater
+        row.meanWADifference = row.wasteDivideAll - row.meanWasteDivideAll
+        return row
+      }
+    })
+
+    dv.transform({
+      type: 'map',
+      callback(row) {
+        row.meanWEDifferencePercent = row.meanWEDifference / row.meanWasteDivideElectricity * 100
+        row.meanWWDifferencePercent = row.meanWWDifference / row.meanWasteDivideWater * 100
+        row.meanWADifferencePercent = row.meanWADifference / row.meanWasteDivideAll * 100
         return row
       }
     })
@@ -138,6 +178,57 @@ class App extends Component {
             type="interval"
             position="year*wasteDivieAll"
             color={['year', '#4C9AFF']}
+          />
+        </Chart>
+
+        <Chart height={400} data={dv} forceFit>
+          <ChartTitle>危废电量比较均值变化百分比</ChartTitle>
+          <Legend />
+          <Axis name="year" />
+          <Axis name="meanWEDifferencePercent" />
+          <Tooltip
+            crosshairs={{
+              type: "y"
+            }}
+          />
+          <Geom
+            type="interval"
+            position="year*meanWEDifferencePercent"
+            color={['year', '#36B37E']}
+          />
+        </Chart>
+
+        <Chart height={400} data={dv} forceFit>
+          <ChartTitle>危废水量比较均值变化百分比</ChartTitle>
+          <Legend />
+          <Axis name="year" />
+          <Axis name="meanWWDifferencePercent" />
+          <Tooltip
+            crosshairs={{
+              type: "y"
+            }}
+          />
+          <Geom
+            type="interval"
+            position="year*meanWWDifferencePercent"
+            color={['year', '#FFE380']}
+          />
+        </Chart>
+
+        <Chart height={400} data={dv} forceFit>
+          <ChartTitle>危废水电总量比较均值变化百分比</ChartTitle>
+          <Legend />
+          <Axis name="year" />
+          <Axis name="meanWADifferencePercent" />
+          <Tooltip
+            crosshairs={{
+              type: "y"
+            }}
+          />
+          <Geom
+            type="interval"
+            position="year*meanWADifferencePercent"
+            color={['year', '#FF7452']}
           />
         </Chart>
       </Wrapper>
