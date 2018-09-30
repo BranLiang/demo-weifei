6f331e3aaef0c731e163c53724ae940eda5da844
Bran Liang <lby89757@hotmail.com>
2018-09-30 10:02:35 +0800

    Update daji data

--- src/data/daji.js
+++ src/data/daji.js
@@ -4,16 +4,19 @@ const data = [
   {
     year: '2015年',
     electricity: 14706311,
+    water: 245928,
     waste: 3173.63
   },
   {
     year: '2016年',
     electricity: 16146210,
+    water: 130997,
     waste: 4363.73
   },
   {
     year: '2017年',
     electricity: 9071264,
+    water: 123389,
     waste: 3465.77
   }
 ]
--- src/data/dajiM.js
+++ src/data/dajiM.js
@@ -4,191 +4,229 @@ const data = [
   {
     year: '2015-06',
     electricity: 2379162,
+    water: 21651,
     waste: 86.16
   },
   {
     year: '2015-07',
     electricity: 1239532,
+    water: 22800,
     waste: 282.71
   },
   {
     year: '2015-08',
     electricity: 1168882,
+    water: 22800,
     waste: 340.74
   },
   {
     year: '2015-09',
     electricity: 91669,
+    water: 25221,
     waste: 219.27
   },
   {
     year: '2015-10',
     electricity: 934996,
+    water: 25221,
     waste: 346.75
   },
   {
     year: '2015-11',
     electricity: 2126884,
+    water: 11472,
     waste: 241.12
   },
   {
     year: '2015-12',
     electricity: 1352344,
+    water: 11472,
     waste: 320.96
   },
   {
     year: '2016-01',
     electricity: 1453290,
+    water: 10341,
     waste: 81.84
   },
   {
     year: '2016-02',
     electricity: 892670,
+    water: 10341,
     waste: 81.45
   },
   {
     year: '2016-03',
     electricity: 86987,
+    water: 7532,
     waste: 79.99
   },
   {
     year: '2016-04',
     electricity: 1327250,
+    water: 7532,
     waste: 200.69
   },
   {
     year: '2016-05',
     electricity: 1339558,
+    water: 9410,
     waste: 151.37
   },
   {
     year: '2016-06',
     electricity: 1448391,
+    water: 9410,
     waste: 704.42
   },
   {
     year: '2016-07',
     electricity: 2919580,
+    water: 9585,
     waste: 880.12
   },
   {
     year: '2016-08',
     electricity: 1507170,
+    water: 9585,
     waste: 329.4
   },
   {
     year: '2016-09',
     electricity: 1352612,
+    water: 19960,
     waste: 480.65
   },
   {
     year: '2016-10',
     electricity: 65977,
+    water: 19960,
     waste: 552.3
   },
   {
     year: '2016-11',
     electricity: 1386226,
+    water: 8670,
     waste: 500.51
   },
   {
     year: '2016-12',
     electricity: 1298099,
+    water: 8670,
     waste: 322.09
   },
   {
     year: '2017-01',
     electricity: 1178328,
+    water: 8940,
     waste: 846.94
   },
   {
     year: '2017-02',
     electricity: 856491,
+    water: 8940,
     waste: 2.06
   },
   {
     year: '2017-03',
     electricity: 758949,
+    water: 9220,
     waste: 201.34
   },
   {
     year: '2017-04',
     electricity: 931174,
+    water: 9220,
     waste: 158.45
   },
   {
     year: '2017-05',
     electricity: 614701,
+    water: 10310,
     waste: 50.33
   },
   {
     year: '2017-06',
     electricity: 664119,
+    water: 10310,
     waste: 187.04
   },
   {
     year: '2017-07',
     electricity: 1567424,
+    water: 12310,
     waste: 280.13
   },
   {
     year: '2017-08',
     electricity: 1042506,
+    water: 12310,
     waste: 1.02
   },
   {
     year: '2017-09',
     electricity: 110222,
+    water: 13719,
     waste: 380.18
   },
   {
     year: '2017-10',
     electricity: 740155,
+    water: 13719,
     waste: 887.45
   },
   {
     year: '2017-11',
     electricity: 392649,
+    water: 7195,
     waste: 148.57
   },
   {
     year: '2017-12',
     electricity: 1282946,
+    water: 7195,
     waste: 183.56
   },
   {
     year: '2018-01',
     electricity: 91856,
+    water: 7600,
     waste: 260
   },
   {
     year: '2018-02',
     electricity: 941372,
+    water: 7600,
     waste: 252.58
   },
   {
     year: '2018-03',
     electricity: 590956,
+    water: 5020,
     waste: 22.3
   },
   {
     year: '2018-04',
     electricity: 72953,
+    water: 5020,
     waste: 88.07
   },
   {
     year: '2018-05',
     electricity: 465270,
+    water: 6083,
     waste: 447.5
   },
   {
     year: '2018-06',
     electricity: 842492,
+    water: 6083,
     waste: 669.73
   },
   {
     year: '2018-07',
     electricity: 2027847,
+    water: 9232,
     waste: 148.27
   }
 ]
